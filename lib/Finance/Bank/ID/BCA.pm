fe427e5a739df58ef632075d5c62225d6e3e06f2
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-12 23:44:27 +0700

    Fix recent connection reset from server issue due to rejection of TE request header (thanks Gisle Aas, ref: https://github.com/libwww-perl/libwww-perl/issues/27)

--- lib/Finance/Bank/ID/BCA.pm
+++ lib/Finance/Bank/ID/BCA.pm
@@ -3,6 +3,7 @@ package Finance::Bank::ID::BCA;
 use 5.010;
 use Moo;
 use DateTime;
+use Log::Any '$log';
 
 # VERSION
 
@@ -16,6 +17,19 @@ sub BUILD {
 
     $self->site("https://ibank.klikbca.com") unless $self->site;
     $self->https_host("ibank.klikbca.com")   unless $self->https_host;
+}
+
+sub _req {
+    my ($self, @args) = @_;
+
+    # 2012-03-12 - KlikBCA server since a few week ago rejects TE request
+    # header, so we do not send them.
+    local @LWP::Protocol::http::EXTRA_SOCK_OPTS =
+        @LWP::Protocol::http::EXTRA_SOCK_OPTS;
+    push(@LWP::Protocol::http::EXTRA_SOCK_OPTS, SendTE => 0);
+    #$log->tracef("EXTRA_SOCK_OPTS=%s", \@LWP::Protocol::http::EXTRA_SOCK_OPTS);
+
+    $self->SUPER::_req(@args);
 }
 
 sub login {
